e27ee48579e97bb46ce6f1b09dc8fddc8b90f903
recall <tk657309822@gmail.com>
2014-05-05 18:09:02 +0800

    Update versioncheck.py
    1、判断python 版本，必须大于2.6 且 小于 3
    2、导入gzip ssl sqlite3 zlib模块

--- lib/utils/versioncheck.py
+++ lib/utils/versioncheck.py
@@ -7,11 +7,15 @@ See the file 'doc/COPYING' for copying permission
 
 import sys
 
+# sys.version => '2.7.3 (default, Jan  2 2013, 13:56:14) \n[GCC 4.7.2]'
 PYVERSION = sys.version.split()[0]
 
+# 判断python 执行版本
 if PYVERSION >= "3" or PYVERSION < "2.6":
     exit("[CRITICAL] incompatible Python version detected ('%s'). For successfully running sqlmap you'll have to use version 2.6 or 2.7 (visit 'http://www.python.org/download/')" % PYVERSION)
 
+
+# 导入 gzip ssl sqlite3 zlib 模块
 extensions = ("gzip", "ssl", "sqlite3", "zlib")
 try:
     for _ in extensions:
